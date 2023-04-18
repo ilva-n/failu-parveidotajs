80622af0e0d66c7423f8eb11320e18439180fd5a
Ilva Neparte <ilva.neparte@gmail.com>
2023-04-18 17:20:50 +0300

    meklēs vienādās koordinātas pa visu tabulu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const selectedFile = document.getElementById("fileInput");
 let failaVeids = "";
+let found;
 let pazinojums = document.getElementById("pazinojumuPanelis");
 function setTableH(){
     // create table heading elements and insert into document
@@ -53,48 +54,58 @@ function handleFiles() {
                 const features = readyContent.features;
                 const featureCount = features.length;
                 if (failaVeids === "paraugi"){
-                    for (let i = 0; i < featureCount; i++) {
-                        //visiem izņemto pirmo feature pēc kārtas   
-                        if ((i > 0) && (features[i].properties["Akta nr"] == features[i - 1].properties["Akta nr"]) 
-                            && (features[i].geometry.coordinates[0] == features[i - 1].geometry.coordinates[0]) 
-                            && (features[i].geometry.coordinates[1] == features[i - 1].geometry.coordinates[1])){
-                            // ja tādas pašas koordinātas kā iepriekšējai feature, tad netaisīs jaunu rindu, bet kabinās klāt pie iepriekšējās
-                            const tabbleRows = document.getElementsByTagName("tr");
-                            const l = tabbleRows.length - 1
-                            const lastRow = tabbleRows[l];
-                            rowCells = lastRow.getElementsByTagName("td");
-                            if (features[i].properties["Parauga nr"] && features[i].properties["Parauga nr"] != "-"){
-                                const plusParaugaNr = "; " + features[i].properties["Parauga nr"];
-                                rowCells[1].textContent += plusParaugaNr;
+                    const featureList = []
+                    for (let i = 0; i < featureCount; i++) {                                                                           
+                        found = false;
+                        // ja tādas pašas koordinātas un akta numurs jau būs bijuši iepriekš, tad neta11isīs jaunu rindu, bet kabinās klāt pie iepriekšējās
+                        for (let j = 0; j < featureList.length; j++){
+                            if((features[i].properties["Akta nr"] == featureList[j].akts) 
+                                && (features[i].geometry.coordinates[0] == featureList[j].koord1)
+                                && (features[i].geometry.coordinates[1] == featureList[j].koord2)){
+                                    const tabbleRows = document.getElementsByTagName("tr");
+                                    const taaRinda = tabbleRows[j];
+                                    rowCells = taaRinda.getElementsByTagName("td");
+                                    if (features[i].properties["Parauga nr"] && features[i].properties["Parauga nr"] != "-"){
+                                        const plusParaugaNr = "; " + features[i].properties["Parauga nr"];
+                                        rowCells[1].textContent += plusParaugaNr;
+                                    }
+                                    const plusKokaSuga = "; " + features[i].properties["Koka suga"];
+                                    rowCells[3].textContent += plusKokaSuga;
+                                    found = true;
+                                    break;
                             }
-                            const plusKokaSuga = "; " + features[i].properties["Koka suga"];
-                            rowCells[3].textContent += plusKokaSuga;
-                                //console.log(rowCells[3].textContent);
-                        } else {
-                            const rrow = document.createElement("tr")
-                            tabble.appendChild(rrow);
-                            const td1 = document.createElement("td");
-                            td1.textContent = features[i].properties["Akta nr"]
-                            const td2 = document.createElement("td");
-                            td2.textContent = features[i].properties["Parauga nr"]
-                            const td3 = document.createElement("td");
-                            td3.textContent = features[i].properties["Parbaudes datums"]
-                            const td4 = document.createElement("td");
-                            td4.textContent = features[i].properties["Koka suga"]
-                            const td5 = document.createElement("td");
-                            td5.textContent = features[i].geometry.coordinates[0];
-                            const td6 = document.createElement("td");
-                            td6.textContent = features[i].geometry.coordinates[1];
-                            const td7 = document.createElement("td");
-                            td7.textContent = features[i].properties["Kaitīgie organismi"]
-                            rrow.appendChild(td1);
-                            rrow.appendChild(td2);
-                            rrow.appendChild(td3);
-                            rrow.appendChild(td4);
-                            rrow.appendChild(td5);
-                            rrow.appendChild(td6);
-                            rrow.appendChild(td7);
                         }
+                        if (found == true){
+                            continue;
+                        }
+                        const obj1 = {}
+                        const rrow = document.createElement("tr")
+                        tabble.appendChild(rrow);
+                        const td1 = document.createElement("td");
+                        td1.textContent = features[i].properties["Akta nr"];
+                        obj1.akts = features[i].properties["Akta nr"] ;
+                        const td2 = document.createElement("td");
+                        td2.textContent = features[i].properties["Parauga nr"];
+                        const td3 = document.createElement("td");
+                        td3.textContent = features[i].properties["Parbaudes datums"];
+                        const td4 = document.createElement("td");
+                        td4.textContent = features[i].properties["Koka suga"];
+                        const td5 = document.createElement("td");
+                        td5.textContent = features[i].geometry.coordinates[0];
+                        obj1.koord1 = features[i].geometry.coordinates[0]
+                        const td6 = document.createElement("td");
+                        td6.textContent = features[i].geometry.coordinates[1];
+                        obj1.koord2 = features[i].geometry.coordinates[1]
+                        const td7 = document.createElement("td");
+                        td7.textContent = features[i].properties["Kaitīgie organismi"];
+                        rrow.appendChild(td1);
+                        rrow.appendChild(td2);
+                        rrow.appendChild(td3);
+                        rrow.appendChild(td4);
+                        rrow.appendChild(td5);
+                        rrow.appendChild(td6);
+                        rrow.appendChild(td7);
+                        featureList.push(obj1);                       
                     }
                 }
                 if (failaVeids === "akti"){
